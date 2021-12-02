cb218dda06bce9e0997b0181a9d77139fda1fe33
Tullis, Sean <seantullis0@gmail.com>
2021-12-02 07:13:10 -0700

    feat: Add day2 challenges

---
+++ day2/part2.py
@@ -0,0 +1,40 @@
+import io
+import sys
+
+def main():
+    input_file = sys.argv[1]
+    with io.open(input_file,"r") as f:
+        directions = [line.split(" ") for line in f]
+
+    sub = Submarine()
+    mapping = {
+        "forward": sub.forward,
+        "up": sub.up,
+        "down": sub.down
+    }
+
+    for direction, amount in directions:
+        mapping[direction](int(amount))
+
+    print(sub.x * sub.y)
+
+class Submarine:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.aim = 0
+
+    def forward(self, distance):
+        self.x += distance
+        self.y += distance * self.aim
+
+    def down(self, depth):
+        self._change_depth(depth)
+
+    def up(self, depth):
+        self._change_depth(-1 * depth)
+
+    def _change_depth(self, change):
+        self.aim += change
+
+main()
